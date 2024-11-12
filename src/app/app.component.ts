ac1ba11c961b2a5677a010ea2534b2712c9f92d9
gihan-aj <gihanakila16@gmail.com>
2024-11-12 10:59:24 +0530

    basic ui and theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopBarComponent } from './layout/top-bar/top-bar.component';
+import { SidenavComponent } from './layout/sidenav/sidenav.component';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopBarComponent, SidenavComponent, MatSidenavModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'transactly-app';
+  screenWidth = signal<number>(window.innerWidth);
+  sideNavbarOpened = signal<boolean>(true);
+
+  ngOnInit(): void {
+    this.sideNavbarOpened.set(this.screenWidth() > 768);
+  }
+
+  changeSideNavbarOpened($event: boolean) {
+    this.sideNavbarOpened.set($event);
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.screenWidth.set(window.innerWidth);
+    if (this.screenWidth() < 768) {
+      this.sideNavbarOpened.set(false);
+    } else {
+      this.sideNavbarOpened.set(true);
+    }
+  }
 }
