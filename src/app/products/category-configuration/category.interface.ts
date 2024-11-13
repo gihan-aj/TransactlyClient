86c634eec55021fe9d11a39b2d131127c2e667d0
gihan-aj <gihanakila16@gmail.com>
2024-11-14 02:57:55 +0530

    Environment variables and some shared models added

---
+++ src/app/products/category-configuration/category.interface.ts
@@ -0,0 +1,13 @@
+export interface CategoryInterface {
+  id: string;
+  name: string;
+  description: string;
+  isActive: boolean;
+}
+
+// can have string operators
+type GUID = string & { isGuid: true };
+function guid(guid: string): GUID {
+  // validation
+  return guid as GUID;
+}
